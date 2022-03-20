9511b583d196ff06879abab0cbe45e30fe7a76ab
Kasutaja Kasutaja <ajurgens@tktk.ee>
2022-03-20 20:11:23 +0200

    StringAndArrays

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kontrollib andmete pikkust
+            //programm kuvab, kumb nendest on pikem, kas ees- või perekonnanimi
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            if(firstName.Length < lastName.Length)
+            {
+                Console.WriteLine($"Sinu perekonnanimi on pikem. {lastName.Length} sümbolit");
+            }
+            else if(firstName.Length > lastName.Length)
+            {
+                Console.WriteLine($"Sinu eesnimi on pikem. {firstName.Length} sümbolit");
+            }
+            else
+            {
+                Console.WriteLine($"Sinu ees- ja perekonnanimi on ühepikkused. {firstName.Length} sümbolit");
+            }
+
+
+            Console.WriteLine("Kena päeva!");
+
+
+
+
+
+        }
+    }
+}
---
+++ HOLCounter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program kuvab konsoolis, mitu 'h', 'o' ja 'l' tähte on lauses "Hello World!"
+            //luua vähemalt 3 counter-it
+
+            Console.WriteLine("Hello World!");
+            string fullSentence = "Hello World!".ToLower();
+
+            
+            int hCounter = 0;
+            int oCounter = 0;
+            int lCounter = 0; 
+
+            for (int i = 0; i < fullSentence.Length; i++)
+            {
+                if (fullSentence[i] == 'h') //char a = 'a';
+                {
+                    hCounter++;
+                }
+                if (fullSentence[i] == 'o')
+                {
+                    oCounter++;
+                }
+                if (fullSentence[i] == 'l')
+                {
+                    lCounter++;
+                }
+            }
+            if (hCounter != 1)
+            {
+                Console.WriteLine($"Lauses on {hCounter} 'h' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses on {hCounter} 'h' täht");
+            }
+            if (oCounter != 1)
+            {
+                Console.WriteLine($"Lauses on {oCounter} 'o' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses on {oCounter} 'o' täht");
+            }
+            if (lCounter != 1)
+            {
+                Console.WriteLine($"Lauses on {lCounter} 'l' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses on {lCounter} 'l' täht");
+            }
+
+
+
+
+        }
+    }
+}
